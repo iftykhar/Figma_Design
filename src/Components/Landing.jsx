b27701145f17d6b7e8899f45c0d66ddbca909a3b
iftykhar alam <s.m.ifty49@gmail.com>
2024-12-20 19:47:30 +0600

    buttons for brand color

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -32,6 +32,55 @@ const Landing = () => {
                     <p className='text-gray-600 text-xl'><strike>$99.00</strike></p>
                     <p className='text-gray-700 text-2xl'>$79.00</p>
                 </div>
+                <div className="pt-6">
+                    <p className="text-gray-600">I must explain to you how all this mistaken idea of denoun cing ple praising pain was born and I will give you a complete account of the system, and expound the actual teaching.</p>
+                </div>
+                <div className="pt-8 flex gap-10 type-model">
+                    <div className="">
+                        <p className='text-gray-600'>Type</p>
+                        <p className='font-bold text-gray-700 text-xl'>Watch</p>
+                    </div>
+                    <div className="">
+                        <p className='text-gray-600'>Model Number</p>
+                        <p className='font-bold text-gray-700 text-xl'>Forerunner 290XT</p>
+                    </div>
+                </div>
+                <div className="color-tag pt-6">
+                    <p className='font-bold text-gray-700 text-xl'>Band Color</p>
+                    <div className="flex gap-3 pt-4">
+                        <div class="flex items-center">
+                            <button class="relative w-6 h-6 bg-blue-500 text-white rounded-full flex justify-center items-center border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800">
+                              
+                                <span class="absolute inset-0 border-2 border-transparent rounded-full hover:border-blue-700 focus:border-blue-700 active:border-blue-800 transition-all duration-200"></span>
+                                <span class="z-10"></span>
+                            </button>
+                        </div>
+                        <div class="flex items-center">
+                            <button class="relative w-6 h-6 bg-blue-500 text-white rounded-full flex justify-center items-center border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800">
+                              
+                                <span class="absolute inset-0 border-2 border-transparent rounded-full hover:border-blue-700 focus:border-blue-700 active:border-blue-800 transition-all duration-200"></span>
+                                <span class="z-10"></span>
+                            </button>
+                        </div>
+                        <div class="flex items-center">
+                            <button class="relative w-6 h-6 bg-blue-500 text-white rounded-full flex justify-center items-center border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800">
+                              
+                                <span class="absolute inset-0 border-2 border-transparent rounded-full hover:border-blue-700 focus:border-blue-700 active:border-blue-800 transition-all duration-200"></span>
+                                <span class="z-10"></span>
+                            </button>
+                        </div>
+                        <div class="flex items-center">
+                            <button class="relative w-6 h-6 bg-blue-500 text-white rounded-full flex justify-center items-center border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800">
+                              
+                                <span class="absolute inset-0 border-2 border-transparent rounded-full hover:border-blue-700 focus:border-blue-700 active:border-blue-800 transition-all duration-200"></span>
+                                <span class="z-10"></span>
+                            </button>
+                        </div>
+
+
+
+                    </div>
+                </div>
             </div>
         </section>
     </>
