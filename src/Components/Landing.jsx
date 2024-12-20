52debaa1860c4c1f09616ba3520f55e90c908f5e
iftykhar alam <s.m.ifty49@gmail.com>
2024-12-20 20:49:26 +0600

    landing page done and responsive done

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,111 +1,84 @@
-import React from 'react'
+// import React from 'react';
 import violate from '../assets/violate.png';
-import { FaStar } from "react-icons/fa";
-import { FaStarHalfAlt } from "react-icons/fa";
-import { FaRegStar } from "react-icons/fa";
+import { FaStar, FaStarHalfAlt, FaRegStar, FaRegHeart } from "react-icons/fa";
 
 const Landing = () => {
   return (
     <>
-      <section className='container flex flex-wrap'>
-        
-            <div className=" w-6/12  items-center">
-                <img className='w-[100%]' src={violate}  alt="" />
+      <section className='container mx-auto p-4 flex flex-wrap'>
+        <div className="w-full md:w-6/12 flex justify-center items-center">
+          <img className='w-full' src={violate} alt="Smart watch" />
+        </div>
+        <div className="w-full md:w-6/12 text-start px-4 py-4">
+          <div className="py-3">
+            <h1 className='text-2xl md:text-4xl'>Classy Modern Smart watch</h1>
+          </div>
+          <div className="ratings flex flex-wrap items-center gap-2 py-2">
+            <div className="star-logo flex text-xl gap-1">
+              <FaStar className='text-yellow-400' />
+              <FaStar className='text-yellow-400' />
+              <FaStar className='text-yellow-400' />
+              <FaStarHalfAlt className='text-yellow-400' />
+              <FaRegStar className='text-yellow-400' />
             </div>
-            <div className="w-6/12 items-start text-start px-10 py-4">
-                <div className="py-3">
-                    <h1 className='text-4xl text-start'>Classy Modern Smart watch</h1>
-                </div>
-                <div className="ratings flex flex-row items-center text-start gap-2 py-2 ">
-                    <div className="star-logo flex text-xl gap-1">
-                        <FaStar className='text-yellow-400' />
-                        <FaStar className='text-yellow-400' />
-                        <FaStar className='text-yellow-400' />
-                        <FaStarHalfAlt className='text-yellow-400' />
-                        <FaRegStar className='text-yellow-400 ' />
-                    </div>
-                    <div className="items-center">
-                        <p className='text-gray-500'>(2 Reviews)</p>
-                    </div>
-                </div>
-                <div className="flex gap-2 items-center">
-                    <p className='text-gray-600 text-xl'><strike>$99.00</strike></p>
-                    <p className='text-gray-700 text-2xl'>$79.00</p>
-                </div>
-                <div className="pt-6">
-                    <p className="text-gray-600">I must explain to you how all this mistaken idea of denoun cing ple praising pain was born and I will give you a complete account of the system, and expound the actual teaching.</p>
-                </div>
-                <div className="pt-8 flex gap-10 type-model">
-                    <div className="">
-                        <p className='text-gray-600'>Type</p>
-                        <p className='font-bold text-gray-700 text-xl'>Watch</p>
-                    </div>
-                    <div className="">
-                        <p className='text-gray-600'>Model Number</p>
-                        <p className='font-bold text-gray-700 text-xl'>Forerunner 290XT</p>
-                    </div>
-                </div>
-                <div className="color-tag pt-6">
-                    <p className='font-bold text-gray-700 text-xl'>Band Color</p>
-                    <div className="flex gap-3 pt-4">
-                        <div class="flex items-center">
-                            <button class="relative w-6 h-6 bg-blue-500 text-white rounded-full flex justify-center items-center border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800">
-                              
-                                <span class="absolute inset-0 border-2 border-transparent rounded-full hover:border-blue-700 focus:border-blue-700 active:border-blue-800 transition-all duration-200"></span>
-                                <span class="z-10"></span>
-                            </button>
-                        </div>
-                        <div class="flex items-center">
-                            <button class="relative w-6 h-6 bg-blue-500 text-white rounded-full flex justify-center items-center border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800">
-                              
-                                <span class="absolute inset-0 border-2 border-transparent rounded-full hover:border-blue-700 focus:border-blue-700 active:border-blue-800 transition-all duration-200"></span>
-                                <span class="z-10"></span>
-                            </button>
-                        </div>
-                        <div class="flex items-center">
-                            <button class="relative w-6 h-6 bg-blue-500 text-white rounded-full flex justify-center items-center border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800">
-                              
-                                <span class="absolute inset-0 border-2 border-transparent rounded-full hover:border-blue-700 focus:border-blue-700 active:border-blue-800 transition-all duration-200"></span>
-                                <span class="z-10"></span>
-                            </button>
-                        </div>
-                        <div class="flex items-center">
-                            <button class="relative w-6 h-6 bg-blue-500 text-white rounded-full flex justify-center items-center border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800">
-                              
-                                <span class="absolute inset-0 border-2 border-transparent rounded-full hover:border-blue-700 focus:border-blue-700 active:border-blue-800 transition-all duration-200"></span>
-                                <span class="z-10"></span>
-                            </button>
-                        </div>
-
-
-
-                    </div>
-                </div>
-                <div className="writs-watch pt-8">
-                    <p className='font-bold text-gray-700 text-xl'>Wrist Size</p>
-                    <div className="size-btns pt-4 flex gap-3">
-                        <button className='border-2 rounded-md py-1 px-6 flex gap-2 text-gray-600 items-center text-center '>
-                            <h2 className='font-semibold'>S</h2><p>$69</p>
-                        </button>
-                        <button className='border-2 rounded-md py-1 px-6 flex gap-2 text-gray-600 items-center text-center '>
-                            <h2 className='font-semibold'>M</h2><p>$79</p>
-                        </button>
-                        <button className='border-2 rounded-md py-1 px-6 flex gap-2 text-gray-600 items-center text-center '>
-                            <h2 className='font-semibold'>L</h2><p>$89</p>
-                        </button>
-                        <button className='border-2 rounded-md py-1 px-6 flex gap-2 text-gray-600 items-center text-center '>
-                            <h2 className='font-semibold'>XL</h2><p>$99</p>
-                        </button>
-                        
-                    </div>
-                </div>
-                <div className="">
-                        <div className=""></div>
-                </div>
+            <p className='text-gray-500'>(2 Reviews)</p>
+          </div>
+          <div className="flex gap-2 items-center">
+            <p className='text-gray-600 text-xl'><strike>$99.00</strike></p>
+            <p className='text-gray-700 text-2xl'>$79.00</p>
+          </div>
+          <div className="pt-6">
+            <p className="text-gray-600">I must explain to you how all this mistaken idea of denoun cing ple praising pain was born and I will give you a complete account of the system, and expound the actual teaching.</p>
+          </div>
+          <div className="pt-8 flex flex-wrap gap-10">
+            <div>
+              <p className='text-gray-600'>Type</p>
+              <p className='font-bold text-gray-700 text-xl'>Watch</p>
             </div>
-        </section>
+            <div>
+              <p className='text-gray-600'>Model Number</p>
+              <p className='font-bold text-gray-700 text-xl'>Forerunner 290XT</p>
+            </div>
+          </div>
+          <div className="pt-6">
+            <p className='font-bold text-gray-700 text-xl'>Band Color</p>
+            <div className="flex gap-3 pt-4">
+              <button className="w-6 h-6 bg-blue-500 rounded-full border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800"></button>
+              <button className="w-6 h-6 bg-blue-500 rounded-full border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800"></button>
+              <button className="w-6 h-6 bg-blue-500 rounded-full border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800"></button>
+              <button className="w-6 h-6 bg-blue-500 rounded-full border-4 border-transparent hover:border-blue-700 focus:border-blue-700 active:border-blue-800"></button>
+            </div>
+          </div>
+          <div className="pt-8">
+            <p className='font-bold text-gray-700 text-xl'>Wrist Size</p>
+            <div className="flex flex-wrap gap-3 pt-4">
+              <button className='border-2 rounded-md py-1 px-6 flex gap-2 text-gray-600 items-center'>
+                <h2 className='font-semibold'>S</h2><p>$69</p>
+              </button>
+              <button className='border-2 rounded-md py-1 px-6 flex gap-2 text-gray-600 items-center'>
+                <h2 className='font-semibold'>M</h2><p>$79</p>
+              </button>
+              <button className='border-2 rounded-md py-1 px-6 flex gap-2 text-gray-600 items-center'>
+                <h2 className='font-semibold'>L</h2><p>$89</p>
+              </button>
+              <button className='border-2 rounded-md py-1 px-6 flex gap-2 text-gray-600 items-center'>
+                <h2 className='font-semibold'>XL</h2><p>$99</p>
+              </button>
+            </div>
+          </div>
+          <div className="pt-10 flex gap-3 items-center">
+            <div className="flex items-center border rounded">
+              <button className="px-4 py-2 border-r">-</button>
+              <span className="px-4 py-2">0</span>
+              <button className="px-4 py-2 border-l">+</button>
+            </div>
+            <button className='border-2 rounded-lg px-4 py-2 bg-blue-500 text-white font-semibold'>Add to Cart</button>
+            <FaRegHeart className='text-xl' />
+          </div>
+        </div>
+      </section>
     </>
   )
 }
 
-export default Landing
+export default Landing;
