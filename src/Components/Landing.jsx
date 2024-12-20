60b286757a73220ef8c2f117cc7683b49d034fbf
iftykhar alam <s.m.ifty49@gmail.com>
2024-12-20 17:39:20 +0600

    image and text column divided

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import violate from '../assets/violate.png';
+import { FaStar } from "react-icons/fa";
+import { FaStarHalfAlt } from "react-icons/fa";
+import { CiStar } from "react-icons/ci";
 
 const Landing = () => {
   return (
     <>
-      <section className='container flex '>
-        {/* <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1> */}
-            <div className=" w-6/12 ">
-                <img src={violate}  alt="" />
+      <section className='container flex flex-wrap'>
+        
+            <div className=" w-6/12  items-center">
+                <img className='w-[100%]' src={violate}  alt="" />
             </div>
-            <div className="w-6/12">
+            <div className="w-6/12 items-center ">
                 <div className="">
-                    <h1 className=''>Classy Modern Smart watch</h1>
+                    <h1 className='text-4xl'>Classy Modern Smart watch</h1>
                 </div>
-                <div className="">
-                    
+                <div className="ratings flex flex-row">
+                    <div className="star-logo flex">
+                        <FaStar className='text-yellow-400' />
+                        <FaStar className='text-yellow-400' />
+                        <FaStar className='text-yellow-400' />
+                        <FaStarHalfAlt className='text-yellow-400' />
+                        <CiStar className='text-yellow-400' />
+                    </div>
+                    <div className="">
+                        <p>(2 Reviews)</p>
+                    </div>
                 </div>
             </div>
         </section>
