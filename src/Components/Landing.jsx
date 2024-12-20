98056a12d983cd959666be3b0ccabdaf130cc7ee
iftykhar alam <s.m.ifty49@gmail.com>
2024-12-20 18:10:09 +0600

    dollar rates

--- src/Components/Landing.jsx
+++ src/Components/Landing.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import violate from '../assets/violate.png';
 import { FaStar } from "react-icons/fa";
 import { FaStarHalfAlt } from "react-icons/fa";
-import { CiStar } from "react-icons/ci";
+import { FaRegStar } from "react-icons/fa";
 
 const Landing = () => {
   return (
@@ -12,21 +12,25 @@ const Landing = () => {
             <div className=" w-6/12  items-center">
                 <img className='w-[100%]' src={violate}  alt="" />
             </div>
-            <div className="w-6/12 items-center ">
-                <div className="">
-                    <h1 className='text-4xl'>Classy Modern Smart watch</h1>
+            <div className="w-6/12 items-start text-start px-10 py-4">
+                <div className="py-3">
+                    <h1 className='text-4xl text-start'>Classy Modern Smart watch</h1>
                 </div>
-                <div className="ratings flex flex-row">
-                    <div className="star-logo flex">
+                <div className="ratings flex flex-row items-center text-start gap-2 py-2 ">
+                    <div className="star-logo flex text-xl gap-1">
                         <FaStar className='text-yellow-400' />
                         <FaStar className='text-yellow-400' />
                         <FaStar className='text-yellow-400' />
                         <FaStarHalfAlt className='text-yellow-400' />
-                        <CiStar className='text-yellow-400' />
+                        <FaRegStar className='text-yellow-400 ' />
                     </div>
-                    <div className="">
-                        <p>(2 Reviews)</p>
+                    <div className="items-center">
+                        <p className='text-gray-500'>(2 Reviews)</p>
                     </div>
+                </div>
+                <div className="flex gap-2 items-center">
+                    <p className='text-gray-600 text-xl'><strike>$99.00</strike></p>
+                    <p className='text-gray-700 text-2xl'>$79.00</p>
                 </div>
             </div>
         </section>
